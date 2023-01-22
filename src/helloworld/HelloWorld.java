5f3fa743fbed9bb12341bf0c1071c02e7d80d7cd
[lynnneeesss] <[hyelin.park@edu.sait.ca]>
2023-01-22 01:31:28 -0700

    New Project Creation Created a new Java Application for HelloWorld.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -27,6 +27,7 @@ public class HelloWorld {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime now = LocalDateTime.now();  
 		
+                
 		return now;
 	}
 		
